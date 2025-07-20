59e08700f1b2258be6a68d160326cf488b6b4a54
izzat530002 <114859029+izzat530002@users.noreply.github.com>
2025-07-20 12:00:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.get("/", async (req, res) => {
   res.json({ links });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
